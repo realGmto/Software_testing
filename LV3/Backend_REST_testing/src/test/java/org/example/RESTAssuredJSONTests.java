2c4a6497e56e4d4225faa1611d1cd12d7065df52
Nikola Muženjak <nmuzenjak@etfos.hr>
2023-11-23 19:39:23 +0100

    Setup RESTAssuredJSONTests class for test cases

--- LV3/Backend_REST_testing/src/test/java/org/example/RESTAssuredJSONTests.java
+++ LV3/Backend_REST_testing/src/test/java/org/example/RESTAssuredJSONTests.java
@@ -1,4 +1,14 @@
 package org.example;
+import static io.restassured.RestAssured.delete;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import org.hamcrest.Matchers;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class RESTAssuredJSONTests {
+    final static String ROOT_URI = "http://localhost:7000/employees";
 }
