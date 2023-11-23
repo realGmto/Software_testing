dc5be253aa65be348b20b67d017a8810e26b05e6
Nikola Muženjak <nmuzenjak@etfos.hr>
2023-11-23 19:52:39 +0100

    Add Test case for GET request

--- LV3/Backend_REST_testing/src/test/java/org/example/RESTAssuredJSONTests.java
+++ LV3/Backend_REST_testing/src/test/java/org/example/RESTAssuredJSONTests.java
@@ -11,4 +11,13 @@ import io.restassured.response.Response;
 
 public class RESTAssuredJSONTests {
     final static String ROOT_URI = "http://localhost:7000/employees";
+
+    @Test
+    public void simple_get_test() {
+        Response response = get(ROOT_URI + "/list");
+        System.out.println(response.asString());
+        response.then().body("id", hasItems(1, 2));
+        response.then().body("name", hasItems("Pankaj"));
+    }
+
 }
